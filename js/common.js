aa60a3dd50b8a733b1f9f2d1fbf399c9a9deee67
j-hossain <jahin.hsn101@gmail.com>
2022-08-10 21:36:50 +0600

    Version 3 [phone number added]

--- js/common.js
+++ js/common.js
@@ -1,2 +1,2 @@
-let teamInfoUrl = "https://script.google.com/macros/s/AKfycbxuRqoLpuLuS2Chzotg3Sa-JoD1aoO_efybFhXO-K5Yl3Y76NjST5r1v-L_Aq3iot-0tw/exec";
+let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyXsrrELVoO3HV1iU1eRBQuJWPxTgyRmrPioYuwoSEg-9IwMq6FjO10-eKPDvEhEvf6yw/exec";
 let teamFinderUrl = "https://script.google.com/macros/s/AKfycbz2t2xNfiC31JBrGLDT_tvEqdQalARglV_uPFVWNUx5NGps7ucsVEuvtBDAvDbdRa52Kw/exec";
