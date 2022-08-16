dd4efb40ba76f42c8e5ff1cfa83a0233d92bfc0b
j-hossain <jahin.hsn101@gmail.com>
2022-08-16 14:00:10 +0600

    Version 6 [Section added]

--- js/common.js
+++ js/common.js
@@ -1,2 +1,2 @@
-let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyjsYy-vwL0FIGQfbcMjS9OUakKu9YIF05tLMXNK6r0lUwJn8Ovir-Jq8pbjt2KpR6X/exec";
+let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyoqvUuZ_OfE7wLHVK7MAh9Paj_cOgYAeR1ZsYBaV_MezRNi8bXQSO2gHKamrXS3RWBNQ/exec";
 let teamFinderUrl = "https://script.google.com/macros/s/AKfycbz2t2xNfiC31JBrGLDT_tvEqdQalARglV_uPFVWNUx5NGps7ucsVEuvtBDAvDbdRa52Kw/exec";
