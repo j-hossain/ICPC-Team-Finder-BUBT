b07096315c82c69c5e971ab829c0861abd033a6f
d-joker <jahin.hsn101@gmail.com>
2023-08-08 15:58:45 +0600

    Coach view functional

--- js/coachView.js
+++ js/coachView.js
@@ -3,32 +3,40 @@ getAllTeam();
 let filterdData = [];
 let AllData = [];
 
-function search() {
-    let value = document.getElementById("search").value;
-    if (value == "") {
-        filterdData = AllData;
-        createTable(filterdData);
-        return;
-    }
-    value = value.toString().toLowerCase();
-    filterdData = [];
-    AllData.forEach(data => {
-        if (data["team_name"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem1_name"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem2_name"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem3_name"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem1_intake"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem2_intake"].toString().toLowerCase().match(value))
-            filterdData.push(data);
-        else if (data["mem3_intake"].toString().toLowerCase().match(value))
-            filterdData.push(data);
+function updateTable() {
+    const teamNameFilter = document.getElementById("teamNameFilter").value.toLowerCase();
+    const sectionFilter = document.getElementById("sectionFilter").value;
+    const intakeFilter = document.getElementById("intakeFilter").value;
+    const registrationStatusFilter = document.getElementById("registrationStatusFilter").value;
+    const coachNameFilter = document.getElementById("coachNameFilter").value;
+
+    filterdData = AllData.filter(team => {
+        return (
+            team["team_name"].toLowerCase().includes(teamNameFilter) &&
+            (registrationStatusFilter === "ALL" || team["reg_status"] === registrationStatusFilter) &&
+            (coachNameFilter === "ALL" || team["Coach"] === coachNameFilter)
+        );
+    });
+    filterdData = filterdData.filter(team => {
+        return checkIntakeSection(intakeFilter, sectionFilter, team);
     });
     createTable(filterdData);
+}
+
+function checkIntakeSection(intakeFilter, sectionFilter, team) {
+    if (sectionFilter === "ALL" && intakeFilter === "ALL") return true;
+    if (sectionFilter === "ALL") return (intakeFilter === "ALL" || team["mem1_intake"] == intakeFilter || team["mem2_intake"] == intakeFilter || team["mem3_intake"] == intakeFilter);
+    if (intakeFilter === "ALL") return (sectionFilter === "ALL" || team["mem1_sec"] == sectionFilter || team["mem2_sec"] == sectionFilter || team["mem3_sec"] == intakeFilter);
+    return ((team["mem1_intake"] == intakeFilter && team["mem1_sec"] == sectionFilter) || (team["mem2_intake"] == intakeFilter && team["mem2_sec"] == sectionFilter) || (team["mem3_intake"] == intakeFilter && team["mem3_sec"] == sectionFilter))
+}
+
+function resetFilter() {
+    document.getElementById("teamNameFilter").value = "";
+    document.getElementById("intakeFilter").value = "ALL";
+    document.getElementById("sectionFilter").value = "ALL";
+    document.getElementById("registrationStatusFilter").value = "ALL";
+    document.getElementById("coachNameFilter").value = "ALL";
+    updateTable();
 }
 
 function getAllTeam() {
@@ -41,11 +49,50 @@ function getAllTeam() {
         console.log(AllData);
         filterdData = AllData;
         createTable(filterdData);
+        populateOptions(AllData);
 
     }
     xhttp.open("GET", teamInfoUrl + "?coachView=true");
     xhttp.send();
 }
+
+function populateOptions(data) {
+    let intakes = new Set();
+    let sections = new Set();
+    let stats = new Set();
+    let coaches = new Set();
+    AllData.forEach(team => {
+        intakes.add(team["mem1_intake"]);
+        intakes.add(team["mem2_intake"]);
+        intakes.add(team["mem3_intake"]);
+        sections.add(team["mem1_sec"]);
+        sections.add(team["mem2_sec"]);
+        sections.add(team["mem3_sec"]);
+        stats.add(team["reg_status"]);
+        coaches.add(team["Coach"]);
+    });
+
+    populateThis(intakes, "intakeFilter");
+    populateThis(sections, "sectionFilter");
+    populateThis(stats, "registrationStatusFilter");
+    populateThis(coaches, "coachNameFilter");
+    document.getElementById("teamNameFilter").addEventListener("input", updateTable);
+}
+
+function populateThis(options, divId) {
+    let div = document.getElementById(divId);
+    div.addEventListener("change", updateTable);
+    div.innerHTML = '<option value="ALL">All</option>';
+    options.forEach(option => {
+        let opt = document.createElement('option');
+        opt.value = option;
+        opt.innerHTML = option == "" ? "Null" : option;
+        div.appendChild(opt);
+    });
+}
+
+
+
 
 function createTable(data) {
     let parent = document.getElementById("listContainerTable");
@@ -68,11 +115,96 @@ function createListItem(data, i) {
     let listItem = document.createElement("tbody");
     listItem.classList.add("teamGroup");
     parent.appendChild(listItem);
-    listItem.innerHTML = '<tr> <td rowspan="3" class=""> <p class="team_name"></p> <span class="phone"></span> </td> <td class="mem1_email"></td> <td class="mem1_name"></td> <td class="mem1_id"></td> <td class="mem1_intake"></td> <td class="mem1_sec"></td> <td rowspan="3"> <p class="reg_status"></p> <button class=".changeBtn"> Change</button> </td> <td rowspan="3" class="Coach"></td> </tr> <tr> <td class="mem2_email"></td> <td class="mem2_name"></td> <td class="mem2_id"></td> <td class="mem2_intake"></td> <td class="mem2_sec"></td> </tr> <tr> <td class="mem3_email"></td> <td class="mem3_name"></td> <td class="mem3_id"></td> <td class="mem3_intake"></td> <td class="mem3_sec"></td> </tr>';
+    listItem.innerHTML = '<tr> <td rowspan="3" class=""> <p class="team_name copyText"></p> <span class="phone copyText"></span> </td> <td class="mem1_email copyText"></td> <td class="mem1_name"></td> <td class="mem1_id"></td> <td class="mem1_intake"></td> <td class="mem1_sec"></td> <td rowspan="3"> <p class="reg_status"></p> <button class="btn"> Change</button> </td> <td rowspan="3" class="Coach"></td> </tr> <tr> <td class="mem2_email copyText"></td> <td class="mem2_name"></td> <td class="mem2_id"></td> <td class="mem2_intake"></td> <td class="mem2_sec"></td> </tr> <tr> <td class="mem3_email copyText"></td> <td class="mem3_name"></td> <td class="mem3_id"></td> <td class="mem3_intake"></td> <td class="mem3_sec"></td> </tr>';
     for (keys in data) {
         listItem.querySelector("." + keys.toString()).innerHTML = data[keys];
     }
-    // listItem.querySelector(".btn").addEventListener("click", function () { loadTeam(i) });
+    let copyEnabled = listItem.querySelectorAll('.copyText');
+    copyEnabled.forEach(text => {
+        text.title = "Click to Copy";
+        text.addEventListener('click', (e) => {
+            let textToCopy = e.target.innerHTML;
+            let copyText = document.createElement('textarea');
+            copyText.value = textToCopy;
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); // For mobile devices
+
+            // Copy the text inside the text field
+            navigator.clipboard.writeText(copyText.value);
+
+            // Alert the copied text
+            showSnackBar("Copied the text: " + copyText.value);
+        })
+    })
+    if (data["reg_status"] == "") {
+        listItem.querySelector(".reg_status").innerHTML = "Null";
+        listItem.classList.add("notyet");
+    }
+    if (data["reg_status"] == "pending") {
+        listItem.classList.add("pending");
+    }
+    listItem.querySelector(".btn").addEventListener("click", function () {
+        if (data["reg_status"] == "") setStatusToPending(i);
+        else if (data["reg_status"] == "pending") setStatusToComplete(i);
+        else {
+            showSnackBar("This team information is already completed");
+        }
+    });
+}
+
+function setStatusToPending(i) {
+    let coachName = prompt("Please enter the Coach Name:");
+    if (coachName != null && coachName != "") {
+        let info = {};
+        for (key in filterdData[i]) {
+            info[key] = filterdData[i][key];
+        }
+        info["reg_status"] = "pending";
+        info["Coach"] = coachName;
+        document.querySelector(".loaderImage").style.display = "block";
+        sendTeam(info, () => {
+            filterdData[i]["reg_status"] = "pending";
+            filterdData[i]["Coach"] = coachName;
+            createTable(filterdData);
+            document.querySelector(".loaderImage").style.display = "none";
+        });
+    }
+}
+
+function sendTeam(info, callback) {
+    fetch(teamInfoUrl + "?coachView=true", {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify(info) // body data type must match "Content-Type" header
+    }).then((e) => {
+        console.log(e);
+        // deletPersonInfo(info);
+        callback();
+
+    }).catch((e) => {
+        console.log(e);
+    });
+}
+
+function setStatusToComplete(i) {
+    if (confirm("Are you sure this team has all the information completed ??\nThe Press ok")) {
+        let info = {};
+        for (key in filterdData[i]) {
+            info[key] = filterdData[i][key];
+        }
+        document.querySelector(".loaderImage").style.display = "block";
+        info["reg_status"] = "complete";
+        sendTeam(info, () => {
+            filterdData[i]["reg_status"] = "complete";
+            createTable(filterdData);
+            document.querySelector(".loaderImage").style.display = "none";
+        });
+    }
 }
 
 function loadTeam(i) {
--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyGOgEAWdXenIN5CuAi1ZUoo4q8aO9YygEl8Y-oeK2zlYd5OY7rS1RMpN3lyprnHIykoA/exec";
-// let teamInfoUrl = "https://script.google.com/macros/s/AKfycbywHLFediK3Ob1A8wbDq5RXhF3OZjMK86T8Oxp8DLFyyRIWrHIQFLyXE99rz-BuZjGk/exec";
+// let teamInfoUrl = "https://script.google.com/macros/s/AKfycbwhI6b9tLhs0cscHQBrT6ASHdcjjMRajOuyFUns7FXnlsZQU2AAMSzaJBYDljONmN4Z/exec";
 let teamFinderUrl = "https://script.google.com/macros/s/AKfycbz2t2xNfiC31JBrGLDT_tvEqdQalARglV_uPFVWNUx5NGps7ucsVEuvtBDAvDbdRa52Kw/exec";
+
+
+function showSnackBar(msg) {
+    let bar = document.getElementById("snackbar");
+    bar.innerHTML = msg;
+    bar.className = "show";
+    setTimeout(function () { bar.className = bar.className.replace("show", ""); }, 3000);
+}
