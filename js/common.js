9fafd53eb1955644c7422d886c4f8f50d11957c0
d-joker <jahin.hsn101@gmail.com>
2023-08-08 10:41:12 +0600

    debug mode off

--- js/common.js
+++ js/common.js
@@ -1,3 +1,3 @@
-// let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyGOgEAWdXenIN5CuAi1ZUoo4q8aO9YygEl8Y-oeK2zlYd5OY7rS1RMpN3lyprnHIykoA/exec";
-let teamInfoUrl = "https://script.google.com/macros/s/AKfycbywHLFediK3Ob1A8wbDq5RXhF3OZjMK86T8Oxp8DLFyyRIWrHIQFLyXE99rz-BuZjGk/exec";
+let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyGOgEAWdXenIN5CuAi1ZUoo4q8aO9YygEl8Y-oeK2zlYd5OY7rS1RMpN3lyprnHIykoA/exec";
+// let teamInfoUrl = "https://script.google.com/macros/s/AKfycbywHLFediK3Ob1A8wbDq5RXhF3OZjMK86T8Oxp8DLFyyRIWrHIQFLyXE99rz-BuZjGk/exec";
 let teamFinderUrl = "https://script.google.com/macros/s/AKfycbz2t2xNfiC31JBrGLDT_tvEqdQalARglV_uPFVWNUx5NGps7ucsVEuvtBDAvDbdRa52Kw/exec";
