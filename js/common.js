b059235b7653ec1c096481154988342925e41823
Jahin Hossain <58515677+j-hossain@users.noreply.github.com>
2024-10-17 01:15:57 +0600

    Update Team info sheet link

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-let teamInfoUrl = "https://script.google.com/macros/s/AKfycbyZwIfPUDVec6RetEgHESWCeQayT9E1hspvPvIOYfHCrdgE8du2pjp9DINFj9pH3cGkDQ/exec";
+let teamInfoUrl = "https://script.google.com/macros/s/AKfycbwLUzKuey0JLYnhKcyCb6YZFI1EAtVp1pUCrwizx-1f5kxNVOAB810Qu7W_2Qm7lzEJ/exec";
 // let teamInfoUrl = "https://script.google.com/macros/s/AKfycbwhI6b9tLhs0cscHQBrT6ASHdcjjMRajOuyFUns7FXnlsZQU2AAMSzaJBYDljONmN4Z/exec";
 let teamFinderUrl = "https://script.google.com/macros/s/AKfycbz2t2xNfiC31JBrGLDT_tvEqdQalARglV_uPFVWNUx5NGps7ucsVEuvtBDAvDbdRa52Kw/exec";
 
